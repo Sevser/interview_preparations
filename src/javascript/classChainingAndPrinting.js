da84574d9c29a346aa09911da76720e4f4d6415d
sevser <sevser39@gmail.com>
2023-02-24 13:26:44 +0300

    add link and review printing solution

--- src/javascript/classChainingAndPrinting.js
+++ src/javascript/classChainingAndPrinting.js
@@ -19,35 +19,28 @@ function printStructure will print to console this text, I mean exact formatting
 
 // asnwer
 
+class FormField {
+  type = '';
+  constructor(type) {
+    this.type = type;
+  }
+  toJSON() {
+    return `{ type: '${this.type}' }`;
+  }
+}
+
 class FormBuilder {
-  attributes = {}
   constructor() {}
   addField(field, type) {
-    this.attributes[field] = { type };
+    this[field] = new FormField(type)
     return this;
   }
   printStructure() {
-    const makeReplacer = () => {
-      let isInitial = true;
-    
-      return (key, value) => {
-        if (isInitial) {
-          isInitial = false;
-          return value;
-        }
-        return JSON.stringify(value, (key, value) => {
-          if (typeof value === 'string') {
-            return `'${value}'`;
-          }
-          return value;
-        }).replace(/{/g, '{ ').replace(/}/g, ' }');
-      }
-    }
-    console.log(JSON.stringify(this.attributes, makeReplacer(), 2).replace(/\\\"|\"/g, ''));
+    console.log(JSON.stringify(this, null, 2).replace(/\"|\,/g, ''));
   }
 }
 
 new FormBuilder()
   .addField('name', 'text')
-  .addField('isActive', 'true')
+  .addField('isActive', 'boolean')
   .printStructure()
